6962b9cc9ffabc7732a5ad8755705ee9f68e0949
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-04-15 11:52:32 +0100

    Create navbar component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
-import WordMark from "@/components/WordMark";
+import NavBar from "@/components/NavBar";
+import { createClient } from "@/prismicio";
 
-export default function Header() {
+export default async function Header() {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
   return (
     <div>
-      <WordMark />
+      <NavBar settings={settings} />
     </div>
   );
 }
